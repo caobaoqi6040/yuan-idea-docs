a7e2bfd182529bba5f893b1aea94bfe53d842fe5
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-08-09 12:35:26 +0800

    feat(core): 增加自定义组件 contributors

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -10,72 +10,76 @@
  * 特别的，请不要在两个配置文件中重复配置相同的项，当前文件的配置项会覆盖 `.vuepress/config.ts` 文件中的配置
  */
 
-import { defineThemeConfig } from 'vuepress-theme-plume'
+import {defineThemeConfig} from 'vuepress-theme-plume'
 import navbar from './navbar'
 import notes from './notes'
+import * as path from "node:path";
 
 /**
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+    logo: 'https://theme-plume.vuejs.press/plume.png',
 
-  appearance: true,  // 配置 深色模式
+    appearance: true,  // 配置 深色模式
 
-  social: [
-    { icon: 'github', link: '/' },
-  ],
-  // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
-  // aside: true, // 页内侧边栏， 默认显示在右侧
-  // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
+    social: [
+        {icon: 'github', link: 'https://github.com/caobaoqi6040/yuan-idea-docs'},
+        {icon: 'discord', link: 'https://discord.com/'},
+        {icon: 'x', link: 'https://x.com/'},
+    ],
+    navbarSocialInclude: ['github', 'discord', 'x'],
+    // aside: true, // 页内侧边栏， 默认显示在右侧
+    outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
-  /**
-   * 文章版权信息
-   * @see https://theme-plume.vuejs.press/guide/features/copyright/
-   */
-  // copyright: true,
+    /**
+     * 文章版权信息
+     * @see https://theme-plume.vuejs.press/guide/features/copyright/
+     */
+    copyright: true,
 
-  // prevPage: true,   // 是否启用上一页链接
-  // nextPage: true,   // 是否启用下一页链接
-  // createTime: true, // 是否显示文章创建时间
+    prevPage: true,   // 是否启用上一页链接
+    nextPage: true,   // 是否启用下一页链接
+    createTime: true, // 是否显示文章创建时间
 
-  /* 站点页脚 */
-  // footer: {
-  //   message: 'Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
-  //   copyright: '',
-  // },
+    /* 站点页脚 */
+    footer: {
+        message: 'Power by <a target="_blank" href="https://theme-plume.vuejs.press/">vuepress-theme-plume</a> & <a target="_blank" href="https://github.com/caobaoqi6040/yuan-idea-docs">yuan-idea-docs</a>',
+        copyright: 'Copyright © 2024-present caobaoqi6040',
+    },
 
-  /**
-   * @see https://theme-plume.vuejs.press/config/basic/#profile
-   */
-  profile: {
-    avatar: 'https://theme-plume.vuejs.press/plume.png',
-    name: '想你所想 爱你所爱',
-    description: 'a docs for yuan&amp;idea',
-    // circle: true,
-    // location: '',
-    // organization: '',
-  },
+    /**
+     * @see https://theme-plume.vuejs.press/config/basic/#profile
+     */
+    profile: {
+        avatar: 'caobaoqi6040.jpg',
+        name: '想你所想 爱你所爱',
+        description: 'a docs for yuan&amp;idea',
+        circle: true,
+        location: 'ShanXi, China',
+        organization: 'caobaoqi6040',
+    },
 
-  navbar,
-  notes,
+    navbar,
+    notes,
 
-  /**
-   * 公告板
-   * @see https://theme-plume.vuejs.press/guide/features/bulletin/
-   */
-  // bulletin: {
-  //   layout: 'top-right',
-  //   contentType: 'markdown',
-  //   title: '公告板标题',
-  //   content: '公告板内容',
-  // },
+    /**
+     * 公告板
+     * @see https://theme-plume.vuejs.press/guide/features/bulletin/
+     */
+    bulletin: {
+        layout: 'top-right',
+        lifetime: 'always',
+        title: '🎉 公告 🎉',
+        contentFile: path.join(__dirname, 'bulletin.md'),
+        enablePage: page => page.path === '/',
+    },
 
-  /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
-  // transition: {
-  //   page: true,        // 启用 页面间跳转过渡动画
-  //   postList: true,    // 启用 博客文章列表过渡动画
-  //   appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
-  // },
+    /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
+    transition: {
+        page: true,        // 启用 页面间跳转过渡动画
+        postList: true,    // 启用 博客文章列表过渡动画
+        appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
+    },
 
 })
--- docs/README.md
+++ docs/README.md
@@ -2,21 +2,25 @@
 pageLayout: home
 externalLinkIcon: false
 config:
-  -
-    type: hero
+  - type: hero
     full: true
     background: tint-plate
     hero:
-      name: Theme Plume
-      tagline: VuePress Next Theme
-      text: 一个简约的，功能丰富的 vuepress 文档&博客 主题
+      name: Yuan&IDEA docs
+      text: 你是我唯一无法优化的递归 ~
       actions:
-        -
-          theme: brand
-          text: 博客
+        - theme: brand
+          text: 开始
           link: /blog/
-        -
-          theme: alt
+        - theme: alt
           text: Github →
-          link: https://github.com/pengzhanbo/vuepress-theme-plume
+          link: https://github.com/caobaoqi6040/yuan-idea-docs
+  - type: custom
 ---
+
+## a
+
+<CardGrid>
+  <RepoCard repo="vuepress/core" />
+  <RepoCard repo="vuepress/ecosystem" />
+</CardGrid>
